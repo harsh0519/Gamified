ae86b31178fae14e4ec22fff3728f85ea61f50f4
Shobhnik13 <shobhnikw@gmail.com>
2023-02-28 18:16:09 +0530

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Features from './Components/Features'
-
+import { Route } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Features/>
+      <Routes>
+        <Route path='/*' element={<Features/>}/>
+      </Routes>
     </div>
   )
 }
